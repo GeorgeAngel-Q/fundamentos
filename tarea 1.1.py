7d59c2dcd3bb335e470804cadbd3c417a45547c6
GeorgeAngel-Q <119142102+GeorgeAngel-Q@users.noreply.github.com>
2022-12-10 08:54:39 -0500

    Update tarea 1.1.py

--- tarea 1.1.py
+++ tarea 1.1.py
@@ -1,5 +1,5 @@
 
 if __name__ == '__main__':
-	print "Opci�n 1:  Cebiche de conchas negras      ---- s/42"
-	print "Opci�n 2:  Cebiche de pescado a la crema  ---- s/39"
-	print "Opci�n 3:  Chicharron de calamar          ---- s/43"
+	print "Opcion 1:  Cebiche de conchas negras      ---- s/42"
+	print "Opcion 2:  Cebiche de pescado a la crema  ---- s/39"
+	print "Opcion 3:  Chicharron de calamar          ---- s/43"
