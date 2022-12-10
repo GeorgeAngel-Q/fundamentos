5caae18ab4b7de7272b2ef72606c52d296ddab8c
GeorgeAngel-q <2022016381@unfv.edu.pe>
2022-12-10 08:18:27 -0500

    trabajo 1

---
+++ tarea 1.1.py
@@ -0,0 +1,5 @@
+
+if __name__ == '__main__':
+	print "Opci�n 1:  Cebiche de conchas negras      ---- s/42"
+	print "Opci�n 2:  Cebiche de pescado a la crema  ---- s/39"
+	print "Opci�n 3:  Chicharron de calamar          ---- s/43"
